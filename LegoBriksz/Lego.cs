2ec1ca46eb1c68eef8d79cb0167ece8e7f0e6334
gajdos.csanad <20.gajdos.csanad@mechwart.com>
2024-09-06 13:12:31 +0200

    Kesz a alap

---
+++ LegoBriksz/Lego.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegoBriksz
+{
+    internal class Lego
+    {
+        //ItemID, ItemName, CategoryName, ColorName, Qty
+        string ItemId;
+        string ItemName;
+        string CategoryName;
+        string ColorName;
+        int Qty;
+
+        public Lego(string beolvas)
+        {
+            string[] feloszt = beolvas.Split(';');
+            ItemId = feloszt[0];
+            ItemName = feloszt[1];
+            CategoryName = feloszt[2];
+            ColorName = feloszt[3];
+            Qty = int.Parse(feloszt[4]);
+        }
+
+        public string ItemId1 { get => ItemId; set => ItemId = value; }
+        public string ItemName1 { get => ItemName; set => ItemName = value; }
+        public string CategoryName1 { get => CategoryName; set => CategoryName = value; }
+        public string ColorName1 { get => ColorName; set => ColorName = value; }
+        public int Qty1 { get => Qty; set => Qty = value; }
+    }
+}
--- LegoBriksz/MainWindow.xaml.cs
+++ LegoBriksz/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace LegoBriksz
 {
@@ -16,9 +18,85 @@ namespace LegoBriksz
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Lego> kockak = [];
         public MainWindow()
         {
             InitializeComponent();
+            tbNev.TextChanged += (s, e) =>
+            {
+                if (kockak.Count > 0)
+                {
+                    if (tbId.Text != "" && tbNev.Text != "")
+                    {
+                        dgAdatok.ItemsSource = kockak.Where(x => x.ItemId1.StartsWith($"{tbId.Text}") &&
+                            x.ItemName1.ToLower().StartsWith($"{tbNev.Text.ToLower()}"));
+
+                    }
+                    else
+                    {
+                        dgAdatok.ItemsSource = kockak.Where(x=> x.ItemName1.ToLower().StartsWith($"{tbNev.Text.ToLower()}"));
+                    }
+
+                }
+                else 
+                {
+                    MessageBox.Show("Nincs beolvasva adatbázis!");
+                }
+            };
+
+            tbId.TextChanged += (s, e) =>
+            {
+                if (kockak.Count > 0)
+                {
+                    if (tbId.Text != "")
+                    {
+                        dgAdatok.ItemsSource = kockak.Where(x => x.ItemId1.StartsWith($"{tbId.Text}") && 
+                            x.ItemName1.ToLower().StartsWith($"{tbNev.Text.ToLower()}"));
+
+                    }
+                    else
+                    {
+                        dgAdatok.ItemsSource = kockak.Where(x => x.ItemId1.StartsWith($"{tbId.Text}"));
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Nincs beolvasva adatbázis!");
+                }
+            };
+
+
+        }
+
+        private void btnBeolvas_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == true)
+            {
+            XDocument doc = XDocument.Load(ofd.FileName);
+            foreach (var item in doc.Descendants("Item"))
+            {
+                    kockak.Add(new Lego($"{item.Element("ItemID").Value};{item.Element("ItemName").Value};{item.Element("CategoryName").Value};{item.Element("ColorName").Value};{item.Element("Qty").Value}"));
+            }
+
+            }
+            dgAdatok.ItemsSource = kockak;
+        }
+
+       
+    }
+    public class HeightToRadiusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            double height = (double)value;
+            return height / 2;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return new NotImplementedException();
         }
     }
 }
