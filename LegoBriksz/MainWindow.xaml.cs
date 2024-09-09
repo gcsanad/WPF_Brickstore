59a29bc80d338ce48bdad45ed7f98aaf5bb03c18
you@example.com <you@example.com>
2024-09-09 13:55:42 +0200

    Yes

--- LegoBriksz/MainWindow.xaml.cs
+++ LegoBriksz/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Win32;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -19,17 +20,30 @@ namespace LegoBriksz
     public partial class MainWindow : Window
     {
         List<Lego> kockak = [];
+        
         public MainWindow()
         {
             InitializeComponent();
+            
             tbNev.TextChanged += (s, e) =>
             {
                 if (kockak.Count > 0)
                 {
-                    if (tbId.Text != "" && tbNev.Text != "")
+                    if (tbId.Text != "")
                     {
                         dgAdatok.ItemsSource = kockak.Where(x => x.ItemId1.StartsWith($"{tbId.Text}") &&
                             x.ItemName1.ToLower().StartsWith($"{tbNev.Text.ToLower()}"));
+
+                    }
+                    else if (tbId.Text != "" && cbKat.SelectedIndex != -1)
+                    {
+                        dgAdatok.ItemsSource = kockak.Where(x => x.ItemId1.StartsWith($"{tbId.Text}") &&
+                            x.ItemName1.ToLower().StartsWith($"{tbNev.Text.ToLower()}") && x.CategoryName1 == $"{cbKat.SelectedItem}");
+
+                    }
+                    else if (cbKat.SelectedIndex != -1)
+                    {
+                        dgAdatok.ItemsSource = kockak.Where(x => x.ItemName1.ToLower().StartsWith($"{tbNev.Text.ToLower()}") && x.CategoryName1 == $"{cbKat.SelectedItem}");
 
                     }
                     else
@@ -42,16 +56,28 @@ namespace LegoBriksz
                 {
                     MessageBox.Show("Nincs beolvasva adatbázis!");
                 }
+                
             };
 
             tbId.TextChanged += (s, e) =>
             {
                 if (kockak.Count > 0)
                 {
-                    if (tbId.Text != "")
+                    if (tbNev.Text != "")
                     {
                         dgAdatok.ItemsSource = kockak.Where(x => x.ItemId1.StartsWith($"{tbId.Text}") && 
                             x.ItemName1.ToLower().StartsWith($"{tbNev.Text.ToLower()}"));
+
+                    }
+                    else if (tbNev.Text != "" && cbKat.SelectedIndex != -1)
+                    {
+                        dgAdatok.ItemsSource = kockak.Where(x => x.ItemId1.StartsWith($"{tbId.Text}") &&
+                            x.ItemName1.ToLower().StartsWith($"{tbNev.Text.ToLower()}") && x.CategoryName1 == $"{cbKat.SelectedItem}");
+
+                    }
+                    else if (cbKat.SelectedIndex != -1)
+                    {
+                        dgAdatok.ItemsSource = kockak.Where(x => x.ItemId1.ToLower().StartsWith($"{tbId.Text.ToLower()}") && x.CategoryName1 == $"{cbKat.SelectedItem}");
 
                     }
                     else
@@ -65,6 +91,62 @@ namespace LegoBriksz
                     MessageBox.Show("Nincs beolvasva adatbázis!");
                 }
             };
+            cbKat.SelectionChanged += (s, e) =>
+
+            {
+                if (kockak.Count > 0)
+                {
+                    if (tbId.Text != "")
+                    {
+                        dgAdatok.ItemsSource = kockak.Where(x => x.ItemId1.StartsWith($"{tbId.Text}") &&
+                            x.ItemName1.ToLower().StartsWith($"{tbNev.Text.ToLower()}"));
+
+                    }
+                    else if (tbId.Text != "" && tbNev.Text != "")
+                    {
+                        dgAdatok.ItemsSource = kockak.Where(x => x.ItemId1.StartsWith($"{tbId.Text}") &&
+                            x.ItemName1.ToLower().StartsWith($"{tbNev.Text.ToLower()}") && x.CategoryName1 == $"{cbKat.SelectedItem}");
+
+                    }
+                    else if (tbNev.Text != "")
+                    {
+                        dgAdatok.ItemsSource = kockak.Where(x => x.ItemName1.ToLower().StartsWith($"{tbNev.Text.ToLower()}") && x.CategoryName1 == $"{cbKat.SelectedItem}");
+
+                    }
+                    else if (cbKat.SelectedIndex == -1)
+                    {
+                        if (tbId.Text != "" && tbNev.Text != "")
+                        {
+                            dgAdatok.ItemsSource = kockak.Where(x => x.ItemId1.StartsWith($"{tbId.Text}") &&
+                                x.ItemName1.ToLower().StartsWith($"{tbNev.Text.ToLower()}"));
+
+                        }
+                        else if (tbNev.Text != "")
+                        {
+                            dgAdatok.ItemsSource = kockak.Where(x => x.ItemName1.ToLower().StartsWith($"{tbNev.Text.ToLower()}"));
+
+                        }
+                        else if (tbId.Text != "")
+                        {
+                            dgAdatok.ItemsSource = kockak.Where(x => x.ItemId1.StartsWith($"{tbId.Text}"));
+                        }
+                        else
+                        {
+                            dgAdatok.ItemsSource = kockak;
+                        }
+                    }
+                    else
+                    {
+                        dgAdatok.ItemsSource = kockak.Where(x => x.CategoryName1.ToLower() == $"{cbKat.SelectedItem.ToString().ToLower()}");
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Nincs beolvasva adatbázis!");
+                }
+            };
+
 
 
         }
@@ -72,19 +154,42 @@ namespace LegoBriksz
         private void btnBeolvas_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
-            if (ofd.ShowDialog() == true)
+            try
             {
-            XDocument doc = XDocument.Load(ofd.FileName);
-            foreach (var item in doc.Descendants("Item"))
-            {
-                    kockak.Add(new Lego($"{item.Element("ItemID").Value};{item.Element("ItemName").Value};{item.Element("CategoryName").Value};{item.Element("ColorName").Value};{item.Element("Qty").Value}"));
-            }
+                if (ofd.ShowDialog() == true)
+                {
+                XDocument doc = XDocument.Load(ofd.FileName);
+                foreach (var item in doc.Descendants("Item"))
+                {
+                        kockak.Add(new Lego($"{item.Element("ItemID").Value};{item.Element("ItemName").Value};{item.Element("CategoryName").Value};{item.Element("ColorName").Value};{item.Element("Qty").Value}"));
+                        
+
+
+                }
+
+                }
+                dgAdatok.ItemsSource = kockak;
 
             }
-            dgAdatok.ItemsSource = kockak;
+            catch (Exception)
+            {
+
+                MessageBox.Show("Nem megfelelő fájl lett kiválasztva / nem lett kiválasztva fájl!");
+            }
+            //foreach (var item in kockak.Select(x => x.CategoryName1).Distinct())
+            //{
+            //    cbKat.Items.Add(item);
+            //};
+            cbKat.ItemsSource = kockak.OrderBy(x => x.CategoryName1).Select(x => x.CategoryName1).Distinct();
+            
         }
 
-       
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            cbKat.SelectedIndex = -1;
+            
+
+        }
     }
     
 }
